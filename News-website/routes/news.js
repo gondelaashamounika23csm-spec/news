79fca73754ea8d286382298c78b25d94ec3b51ff
pradeep <narupradeep001@gmail.com>
2025-10-24 09:04:35 +0530

    Updated category version

--- News-website/routes/news.js
+++ News-website/routes/news.js
@@ -2,18 +2,20 @@ const express = require('express');
 const axios = require('axios');
 const newsr = express.Router();
 const moment = require('moment');
-const math = require('math');
 
+// Home route - Top headlines for India
 newsr.get('/', async (req, res) => {
   try {
     const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=36f3e29b704f41339af8439dc1228334`;
     const news_get = await axios.get(url);
     res.render('news', { articles: news_get.data.articles });
   } catch (error) {
-    if (error.response) console.log(error);
+    console.log(error);
+    res.render('news', { articles: [] });
   }
 });
 
+// Search route
 newsr.post('/search', async (req, res) => {
   const search = req.body.search;
   try {
@@ -21,18 +23,30 @@ newsr.post('/search', async (req, res) => {
     const news_get = await axios.get(url);
     res.render('news', { articles: news_get.data.articles });
   } catch (error) {
-    if (error.response) console.log(error);
+    console.log(error);
+    res.render('news', { articles: [] });
   }
 });
 
+// Category route
 newsr.get('/:category', async (req, res) => {
-  const category = req.params.category;
+  const category = req.params.category.toLowerCase();
+
+  // Valid categories for NewsAPI
+  const validCategories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+
+  if (!validCategories.includes(category)) {
+    // Invalid category - show empty
+    return res.render('news', { articles: [] });
+  }
+
   try {
     const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=36f3e29b704f41339af8439dc1228334`;
     const news_get = await axios.get(url);
     res.render('news', { articles: news_get.data.articles });
   } catch (error) {
-    if (error.response) console.log(error);
+    console.log(error);
+    res.render('news', { articles: [] });
   }
 });
 
